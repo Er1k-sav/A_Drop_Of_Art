3508d3fe7b38948a4c426d84a296bb18ec3d8235
Er1k-sav <71546251+Er1k-sav@users.noreply.github.com>
2024-01-24 14:29:02 +0000

    ok

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "main.html");
-})
+    res.type("html");
+});
+
+app.get("/style.css", (req, res) => {
+    res.sendFile(__dirname + "style.css");
+    res.type("css");
+});
 
 app.listen(8080);
