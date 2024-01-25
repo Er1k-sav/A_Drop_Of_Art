18d3d01505c877cb1c3232c4fe39f8b272d1b23f
Er1k-sav <71546251+Er1k-sav@users.noreply.github.com>
2024-01-25 14:58:41 +0100

    fix

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const $button = document.querySelector("#getstartedbt");
-const { clientHeight, clientWidth } = $button;
+const button = document.querySelector("#getstartedbt");
+const { clientHeight, clientWidth } = button;
 const r = Math.sqrt(clientHeight ** 2 + clientWidth ** 2);
 
-$button.addEventListener("click", (e) => {
+button.addEventListener("click", (e) => {
   const $ripple = document.createElement("span");
   $ripple.className = "button-ripple";
   $ripple.style.height = `${2 * r}px`;
@@ -10,8 +10,20 @@ $button.addEventListener("click", (e) => {
   $ripple.style.top = `${e.offsetY - r}px`;
   $ripple.style.left = `${e.offsetX - r}px`;
 
-  const $last = $button.querySelector(".button-ripple");
+  const $last = button.querySelector(".button-ripple");
   if ($last) $last.remove();
 
-  $button.appendChild($ripple);
+  button.appendChild($ripple);
+
+  
+  const arrow = document.getElementById("arrowsvg");
+
+  const timer = setInterval(() => {
+    arrow.style.top = arrow.style.top + 1;
+  }, 1);
+
+  setTimeout(() => {
+    timer.clearInterval();
+    button.opacity = 0;
+  }, 300);
 });
